75281eb3ecbbdcc43366bae380e78fec9755fa71
nimranasir29 <64038512+nimranasir29@users.noreply.github.com>
2024-02-19 14:10:29 +0500

    Profile Website

---
+++ Task 2/script.js
@@ -0,0 +1,20 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+  });
+  
+  // Form submission handling
+  document.getElementById('contact-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    
+    const formData = new FormData(this);
+    console.log('Form data:', Object.fromEntries(formData));
+    // Clear the form inputs
+    this.reset();
+  });
+  
+  
